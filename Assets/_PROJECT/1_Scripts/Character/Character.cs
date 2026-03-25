52f11f40930cf7017343736bc9de193eb57053df
Junho <dlatlrhd@gmail.com>
2026-03-25 17:33:31 +0900

    캐릭터 스크립트 중간저장

--- Assets/_PROJECT/1_Scripts/Character/Character.cs
+++ Assets/_PROJECT/1_Scripts/Character/Character.cs
@@ -5,30 +5,33 @@ using UnityEngine;
 using UnityEngine.AI;
 
 //todo 즉사가 아닐때
-//todo 렉돌
 
 public enum State
 {
-    //대기, 경계, 걷기, 뛰기, 동료부르기, 관찰, 저격위치 발각, 죽음
-    Idle, Careful, Walk, Run, Scream, See, Discover, Dead
+    //대기, 경계, 걷기, 뛰기, 동료부르기, 관찰, 저격위치 발각, 상처, 죽음
+    Idle, Careful, Walk, Run, Scream, See, Discover, Hurt, Dead
 }
 
 public class Character : MonoBehaviour
 {
     [Header("순찰 위치")]
     public Vector3[] destinationsPos; // 자동 이동 지정위치
+    [Header("시야범위")]
+    public Collider viewCol; //감지용 콜라이더
     [Header("시야각")]
-    public float viewAngle = 90f;
+    public float viewAngle = 90f; //색적범위
     [Header("관찰대상 레이어")]
-    public LayerMask Evidence;
+    public LayerMask Evidence; //색적대상
+    [Header("상처 지속 시간")] 
+    public float deathLimitTime = 10f; //과다출혈 사망시간
     
-    private Collider col; //감지용 콜라이더
     private Animator anim; //애니메이터
     private NavMeshAgent agent; //AI
     
     private State curState; //현재 상태
     private Array stateValues; //enum개수 체크용
-    private Transform target;
+    private Transform target; //관찰대상
+    private bool isGotShot;
 
     protected virtual void Awake()
     {
@@ -44,7 +47,7 @@ public class Character : MonoBehaviour
         StartCoroutine(MoveCoroutine());
     }
 
-    //행동 루틴
+    #region 기본 코루틴
     protected virtual IEnumerator MoveCoroutine()
     {
         while (true)
@@ -52,108 +55,199 @@ public class Character : MonoBehaviour
             switch (curState)
             {
                 case State.Idle:
+                    ChangeAnim(State.Idle);
                     yield return Idle();
                     break;
                 case State.Careful:
+                    ChangeAnim(State.Careful);
                     yield return Careful();
                     break;
                 case State.Walk:
+                    ChangeAnim(State.Walk);
                     yield return Walk();
                     break;
                 case State.Run:
+                    ChangeAnim(State.Run);
                     yield return Run();
                     break;
                 case State.Scream:
+                    ChangeAnim(State.Scream);
                     yield return Scream();
                     break;
                 case State.See:
+                    ChangeAnim(State.See);
                     yield return See();
                     break;
                 case State.Discover:
-                    yield return Discover();
+                    ChangeAnim(State.Discover);
+                    Discover();
+                    break;
+                case State.Hurt:
+                    ChangeAnim(State.Hurt);
+                    yield return Hurt();
                     break;
                 case State.Dead:
-                    yield return Dead();
+                    ChangeAnim(State.Dead);
+                    Dead();
                     break;
             }
 
             yield return null;
         }
     }
+    #endregion
 
+    #region 행동별 코루틴
     //대기
     protected virtual IEnumerator Idle()
     {
-        //일정시간 후 걷기
-        ChangeAnim(State.Idle);
-        yield return null;
+        //todo 일정시간 후 걷기
+        float time = 0f;
+        while (time < 1)
+        {
+            if (curState == State.Idle) break;
+            time += Time.deltaTime;
+            float t = time / 1;
+            yield return null;
+        }
     }
 
     //경계
     protected virtual IEnumerator Careful()
     {
-        //일정시간 경계 후 대기
-        ChangeAnim(State.Careful);
-        yield return null;
+        //todo 일정시간 경계 후 대기
+        float time = 0f;
+        while (time < 1)
+        {
+            if (curState == State.Careful) break;
+            time += Time.deltaTime;
+            float t = time / 1;
+            yield return null;
+        }
     }
     
     //걷기
     protected virtual IEnumerator Walk()
     {
-        //목표까지 걸어간 후 대기 or 걷는 중에 랜덤확률로 대기
-        ChangeAnim(State.Walk);
-        yield return null;
+        //todo 목표까지 걸어간 후 대기 or 걷는 중에 랜덤확률로 대기
+        float time = 0f;
+        while (time < 1)
+        {
+            if (curState == State.Walk) break;
+            time += Time.deltaTime;
+            float t = time / 1;
+            yield return null;
+        }
     }
 
     //뛰기
     protected virtual IEnumerator Run()
     {
-        //목표까지 뛰어간 후 대기
-        ChangeAnim(State.Run);
-        yield return null;
+        //todo 목표까지 뛰어간 후 대기
+        float time = 0f;
+        while (time < 1)
+        {
+            if (curState == State.Run) break;
+            time += Time.deltaTime;
+            float t = time / 1;
+            yield return null;
+        }
     }
 
     //소리지르기
     protected virtual IEnumerator Scream()
     {
         //일정 시간 소리지른 후 관찰
-        transform.forward = target.position - transform.position;
-        ChangeAnim(State.Scream);
-        yield return null;
+        if (target != null)
+        {
+            transform.forward = target.position - transform.position;
+        }
+        CallFriend();
+        
+        float time = 0f;
+        while (time < 1)
+        {
+            if (curState == State.Scream) break;
+            time += Time.deltaTime;
+            float t = time / 1;
+            yield return null;
+        }
+
+        curState = State.See;
+    }
+
+    //동료 부르기
+    protected virtual void CallFriend()
+    {
+        //todo 범위 내 동료의 이동타겟 바꾸기,강제 이동
     }
     
     //관찰
     protected virtual IEnumerator See()
     {
         //관찰대상 사라질때까지 관찰 후 대기 or 특정 상황 관찰 후 대기
-        transform.forward = target.position - transform.position;
-        ChangeAnim(State.See);
-        yield return null;
+        while (target != null)
+        {
+            if(curState == State.See) break;
+            transform.forward = target.position - transform.position;
+            yield return null;
+        }
+        curState = State.Idle;
     }
 
-    //저격위치 발각
-    protected virtual IEnumerator Discover()
+    //상처
+    protected virtual IEnumerator Hurt()
     {
-        //카메라 클로즈업, 발각 모션 후 게임 오버
+        //자리에서 계속 소리지르기, 동료가 오면 일정시간 후 치료됨, 동료가 안오면 일정시간 후 과다출혈 사망
+        float time = 0f;
+        while (time < deathLimitTime)
+        {
+            time += Time.deltaTime;
+            float t = time / deathLimitTime;
+
+            CallFriend();
+            yield return null;
+        }
+
+        if (time >= deathLimitTime)
+        {
+            curState = State.Dead; //시간 지나면 죽음
+        }
+        else if (isGotShot)
+        {
+            curState = State.Discover; //총에 맞았으면 발각, 게임오버
+        }
+        else
+        {
+            curState = State.Idle;
+        }
+    }
+    
+    //저격위치 발각
+    protected virtual void Discover()
+    {
+        //todo 카메라 클로즈업, 발각 모션 후 게임 오버
         //transform.forward = 플레이어.position - transform.position;
-        ChangeAnim(State.Discover);
-        yield return null;
     }
     
     //죽음
-    protected virtual IEnumerator Dead()
+    protected virtual void Dead()
     {
         //행동 종료, 관찰대상 레이어로 바꾸기
-        ChangeAnim(State.Dead);
-        yield return null;
+        StopAllCoroutines();
+        transform.gameObject.layer = Evidence;
     }
-
+    #endregion
+    
     //애니메이션 상태 바꾸기
     protected virtual void ChangeAnim(State changeState)
     {
         for (int i = 0; i < stateValues.Length; i++)
         {
-            anim.SetBool($"is{(State)i}", changeState == (State)i);
+            if (changeState != State.Dead)
+            {
+                anim.SetBool($"is{(State)i}", changeState == (State)i);
+            }
         }
     }
     
@@ -163,7 +257,23 @@ public class Character : MonoBehaviour
         target = t;
         React();
     }
-
+    
+    //Obstacle 호출 메서드, Obstacle에 맞았을때
+    //Player 호출 메서드, 총에 맞았을때
+    public void Hit(bool isGunShot, Collider hitCollider)
+    {
+        isGotShot = isGunShot;
+        //todo 헤드샷체크
+        // if ()//(hitCollider != null && hitCollider == headCollider)
+        // {
+        //     curState = State.Dead;
+        // }
+        // else
+        // {
+        //     curState = State.Hurt;
+        // }
+    }
+    
     //소리에 대한 반응
     protected virtual void React()
     {
