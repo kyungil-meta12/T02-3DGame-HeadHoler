0caba559ef3f41c34a6ba61aa9140d745eeea1b9
박정민 <pahkjm@gmail.com>
2026-03-25 14:49:34 +0900

    Obstacle 기본 부분 작성

--- Assets/_PROJECT/1_Scripts/Obstacle/Obstacle.cs
+++ Assets/_PROJECT/1_Scripts/Obstacle/Obstacle.cs
@@ -1,10 +1,10 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 public class Obstacle : MonoBehaviour
 {
-    /*
+	/*
     public 필드 //인스펙터에서 관리해야 된다, 다른 스크립트가 호출해야 된다.
     
     private 필드 //자식이 몰라도 부모가 쓰면 된다.
@@ -25,22 +25,56 @@ public class Obstacle : MonoBehaviour
     시민 - 농부, 목수, ... 종류를 여러가지 추가해도 Character만 상속하면 Obstacle과 상호작용가능.
     */
 
+	[SerializeField]
+	private SphereCollider hitSound;	//소리 감지영역 담당 Collider
+	[SerializeField]
+	private float maxSoundRadius = 10f;	//소리 감지영역의 최대반경
+	[SerializeField]
+	private float duration = 1f;		//소리 감지영역이 퍼지는 시간
+
+	private HashSet<Character> heard = new HashSet<Character>();	//범위 내 소리를 들은 Character 체크(중복 방지)
+
     protected virtual void Hit() //총알에 맞았을때
     {
-        
-    }
+		UniqueInteraction();
+		StartCoroutine(SoundCoRoutine());
+	}
 
-    protected virtual void OnColliderEnter(Collider other) //소리범위에 시민이나 적이 닿았을때 Character의 메서드를 호출한다.
+	protected virtual void OnTriggerEnter(Collider other)//소리범위에 시민이나 적이 닿았을때 Character의 메서드를 호출한다.
+	{
+		Character character = other.GetComponent<Character>();	//부딪친 오브젝트가 Character컴포넌트가 있는지 확인
+
+		if (character != null && !heard.Contains(character))	//Character가 있고 아직 소리를 들은 사람에 체크되지 않았으면
+		{
+			heard.Add(character);	//감지된 목록에 추가
+
+			character.HearSound(transform.position);	//Character의 HearSound() 호출
+		}
+	}
+
+	protected virtual IEnumerator SoundCoRoutine() //소리범위를 늘려준다.
     {
-        
-    }
+		heard.Clear();		//들은 사람 체크 초기화
 
-    protected virtual IEnumerator SoundCoRoutine() //소리범위를 늘려준다.
-    {
-        yield return null;
-    }
+		hitSound.radius = 0f;		//소리 감지영역 크기 초기화
+		hitSound.enabled = true;
+		float time = 0f;	//소리 퍼지는 시간 초기화
 
-    protected virtual void UniqueInteraction() //고유한 작용
+
+		while (time < duration)
+		{
+			time += Time.deltaTime;
+			float t = time / duration;
+
+			hitSound.radius = Mathf.Lerp(0f, maxSoundRadius, t);
+			
+			yield return null;
+		}
+
+		hitSound.enabled = false;
+	}
+
+	protected virtual void UniqueInteraction() //고유한 작용
     {
         //깨진다
     }
--- Assets/_PROJECT/1_Scripts/Obstacle/Switchboard.cs
+++ Assets/_PROJECT/1_Scripts/Obstacle/Switchboard.cs
@@ -1,11 +1,34 @@
-using System;
 using UnityEngine;
 
-public class SwitchBoard : Obstacle
+public class SwitchBoard : Obstacle		//건물 배전반
 {
+	[SerializeField]
+	private GameObject connectedBuilding;	//정전되는 건물
+	private bool isBroken = false;		//배전반 파괴여부
+
     protected override void UniqueInteraction() //고유한 작용
     {
-        base.UniqueInteraction(); //깨진다.
-        //정전을 시킨다.
+		if (isBroken)
+		{
+			return;
+		}
+
+		base.UniqueInteraction();
+
+		isBroken = true;
+		BlackOut();		//정전을 시킨다.
+		Debug.Log($"{gameObject.name} 건물이 정전되었습니다");
     }
+
+	private void BlackOut()
+	{
+		if (connectedBuilding != null)
+		{
+			Light[] lights = connectedBuilding.GetComponentsInChildren<Light>();
+			foreach (Light light in lights)
+			{
+				light.enabled = false;
+			}
+		}
+	}
 }
