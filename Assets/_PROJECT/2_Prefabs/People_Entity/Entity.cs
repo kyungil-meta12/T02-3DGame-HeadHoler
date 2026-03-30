cb61762e248dcbd084809837bcfc4240a18cecb2
Junho <dlatlrhd@gmail.com>
2026-03-30 17:41:55 +0900

    Entity 애니메이션 세팅 완료, 랜더러 커스텀 선택 구현 완료

--- Assets/_PROJECT/2_Prefabs/People_Entity/Entity.cs
+++ Assets/_PROJECT/2_Prefabs/People_Entity/Entity.cs
@@ -1,30 +1,81 @@
 using System;
 using System.Collections;
+using UnityEditor.Animations;
 using UnityEngine;
-
+using Unity.Behavior;
 
 
 public class Entity : MonoBehaviour
 {
     private static readonly int AlertLevel = Animator.StringToHash("AlertLevel");
-    
+    [Header("======세팅======")]
     [Header("소속")]
     public Team myTeam;
+    public Role myRole;
+    [Header("성별 선택")]
+    public bool isMale = true;
+    [Header("랜더러 선택")]
+    public int rendererIndex = 0;
+    [Header("머리장비 선택")]
+    public int equipIndex = 99;
+    [Header("순찰 포인트")]
+    public GameObject[] patrolPoints;
     [Header("경계 지속시간")]
     public static float alertTimer = 5f;
+
+    [Space(20)] [Header("======참조======")] 
+    [Header("남성 랜더러")]
+    public Mesh[] maleRenderMeshes; 
+    [Header("여성 랜더러")]
+    public Mesh[] femaleRenderMeshes; 
+    [Header("머리장비")]
+    public GameObject[] equips;
+    [Header("애니메이터 컨트롤러")]
+    public AnimatorController[] animatorControllers;
 
     internal int alertLevel;
     
     private Animator animator;
+    private SkinnedMeshRenderer rend;
+    private BehaviorGraphAgent behaviorGraphAgent;
+    private RagdollController ragdollController;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
+        rend = GetComponentInChildren<SkinnedMeshRenderer>();
+        behaviorGraphAgent = GetComponent<BehaviorGraphAgent>();
+        ragdollController = GetComponent<RagdollController>();
     }
 
     private void Start()
     {
         Sg_GameManager.Inst.entities.Add(this);
+        
+        //소속, 순찰포인트 세팅
+        behaviorGraphAgent.SetVariableValue("Role", myRole);
+        behaviorGraphAgent.SetVariableValue("PatrolPoints", patrolPoints);
+
+        //성별, 인덱스에 맞춰 랜더러 바꾸기
+        if (isMale)
+        {
+            animator.runtimeAnimatorController = animatorControllers[0];
+            int curIndex = rendererIndex % maleRenderMeshes.Length;
+            rend.sharedMesh = maleRenderMeshes[curIndex];
+        }
+        else
+        {
+            animator.runtimeAnimatorController = animatorControllers[1];
+            int curIndex = rendererIndex % femaleRenderMeshes.Length;
+            rend.sharedMesh = femaleRenderMeshes[curIndex];
+        }
+
+        //머리장비 바꾸기
+        if (equipIndex < equips.Length)
+        {
+            Instantiate(equips[equipIndex], ragdollController.headCollider.transform);
+        }
+        
         //StartCoroutine(AlertTimer());
     }
 
--- Assets/_PROJECT/2_Prefabs/People_Entity/Entity_Citizen_Police.cs
+++
@@ -1,5 +0,0 @@
-
-public class Entity_Citizen_Police : Entity_Citizen
-{
-    
-}
