4de9e903c90f3e2088269e29139b8351d6171e9a
TU-PJY <viska0319@icloud.com>
2026-04-07 16:45:48 +0900

    자동차 구간 반복 스크립트 추가

---
+++ Assets/_PROJECT/1_Scripts/Etc/CarIterator.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CarIterator : MonoBehaviour
+{
+    public List<Transform> wayPoints = new();
+    public Transform[] wheels;
+    public float wheelRotationMultiplier = 500f; // 바퀴 회전 배수
+
+    private Vector3 originPosition;
+    private Quaternion originRotation;
+    private int currentIndex;
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        originPosition = transform.position;
+        originRotation = transform.rotation;
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(wayPoints[0].position);
+        agent.autoBraking = false;
+        agent.isStopped = false;
+    }
+
+    void Update()
+    {
+        if (wayPoints.Count == 0)
+        {
+            return;
+        }
+
+        if (!agent.pathPending && agent.remainingDistance < 5f)
+        {
+            SetDestinationToNextPoint();
+        }
+
+        float currentMoveSpeed = agent.velocity.magnitude;
+        foreach (var wheel in wheels)
+        {
+            wheel.Rotate(Vector3.right * currentMoveSpeed * wheelRotationMultiplier * Time.deltaTime);
+        }
+
+        var currentPos = transform.position;
+        currentPos.y = originPosition.y;
+        transform.position = currentPos;
+    }
+
+    void SetDestinationToNextPoint()
+    {
+        if (wayPoints.Count == 0)
+        {
+            return;
+        }
+        if (currentIndex < wayPoints.Count - 1)
+        {
+            agent.destination = wayPoints[currentIndex].position;
+            currentIndex++;
+        }
+        else
+        {
+            transform.position = originPosition;
+            transform.rotation = originRotation;
+            currentIndex = 0;
+            agent.SetDestination(wayPoints[0].position);
+            agent.Warp(originPosition);
+        }
+    }
+}
